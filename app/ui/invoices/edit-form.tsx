e22ed56c09aadb38850599df5ff55a05b9700a11
rajavenkatesh04 <grv.9604@gmail.com>
2025-07-13 17:14:13 +0530

    Chapter 15

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
+import { updateInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 import {
   CheckIcon,
   ClockIcon,
@@ -11,14 +13,17 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
 export default function EditInvoiceForm({
-  invoice,
-  customers,
-}: {
+                                          invoice,
+                                          customers,
+                                        }: {
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const initialState: State = { message: null, errors: {} };
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+  const [state, formAction] = useActionState(updateInvoiceWithId, initialState);
   return (
-    <form>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
